ed2862ce7284bfa4521f756e5336f7b2244a04ae
Justin Voitel <justin.voitel@gmail.com>
2020-05-13 08:58:14 +0200

    weiss nicht

--- postcss.config.js
+++ postcss.config.js
@@ -1,21 +1,22 @@
-const tailwindcss = require("tailwindcss");
+const tailwindcss = require("tailwindcss")
+console.log("node: ", process.env.NODE_ENV)
 
 // only needed if you want to purge
 const purgecss = require("@fullhuman/postcss-purgecss")({
-  content: ["./output/index.html"],
-  /*  whitelist: ['mode-dark'], */
-  defaultExtractor: content => content.match(/[A-Za-z0-9-_:/]+/g) || []
-});
+   content: ["./output/index.html"],
+   /*  whitelist: ['mode-dark'], */
+   defaultExtractor: content => content.match(/[A-Za-z0-9-_:/]+/g) || []
+})
 
 const cssnano = require("cssnano")({
-  preset: "default"
-});
+   preset: "default"
+})
 
 module.exports = {
-  plugins: [
-    //tailwindcss("tailwind.config.js"),
-    tailwindcss(process.env.TAILWIND_CONFIG),
-    // only needed if you want to purge
-    ...[purgecss, cssnano]
-  ]
-};
+   plugins: [
+      //tailwindcss("tailwind.config.js"),
+      tailwindcss(process.env.TAILWIND_CONFIG),
+      // only needed if you want to purge
+      ...(process.env.NODE_ENV === "full" ? [] : [purgecss, cssnano])
+   ]
+}
